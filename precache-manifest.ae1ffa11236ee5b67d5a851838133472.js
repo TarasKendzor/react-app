e04fccbefbca49d4eed4d369a77a89aa1700b150
Taras  Kendzor <45481754+TarasKendzor@users.noreply.github.com>
2019-07-12 14:28:53 +0300

    Updates

---
+++ precache-manifest.ae1ffa11236ee5b67d5a851838133472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31343741a0abf911d45df78890c74ced",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "d64c3c49fcad4a1bc760",
+    "url": "/react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react-app/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d64c3c49fcad4a1bc760",
+    "url": "/react-app/static/js/main.7ef88790.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
